c048d4ec45f3cfe0173f06e22ec81783a947d573
starlaxy <conan.wst@gmail.com>
2023-12-11 08:11:52 +0900

    feat: Expand Blazor App with New UI Components and Pages

---
+++ BlazorApp1/Shared/Item.cs
@@ -0,0 +1,9 @@
+namespace BlazorApp1.Shared
+{
+public class Item
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string ImageUrl { get; set; }
+    public decimal Amount { get; set; }
+}}
---
+++ BlazorApp1/Shared/Robot.cs
@@ -0,0 +1,10 @@
+namespace BlazorApp1.Shared
+{
+    public class Robot
+    {
+        public string RobotId { get; set; }
+        public string Status { get; set; }
+        public string LastActivity { get; set; }
+        public string NextActivity { get; set; }
+    }
+}
